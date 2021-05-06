c38f11b8b606ccf9f854f4461de1c9506ad278dd
tatianaazulay <tania142857@gmail.com>
2021-05-06 18:44:56 -0400

    Add static route

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 //process.env.PORT
 
 //process.env.NODE_ENV => production or undefined
-app.use(express.static(__dirname ))
+
 //middleware
 app.use(cors());
 app.use(express.json());//req.body
